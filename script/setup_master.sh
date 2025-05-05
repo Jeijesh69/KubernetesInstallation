d6e48a23f755c05ce2637a9097fdb2d814ecff2b
Jeijesh <jeshica.anak.baik.hati@gmail.com>
2025-05-05 15:34:39 +0700

    Create setup_master.sh

---
+++ script/setup_master.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Get the private IP address of the current server
+CONTROL_PLANE_IP=$(hostname -I | awk '{print $1}')
+
+# Disable swap temporarily
+sudo swapoff -a
+
+# Create a systemd service to disable swap permanently
+echo -e "[Unit]\nDescription=Disable Swap\n\n[Service]\nType=oneshot\nExecStart=/sbin/swapoff -a\nRemainAfterExit=true\n\n[Install]\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/disable-swap.service
+
+# Reload systemd and enable the service to ensure swap is off on boot
+sudo systemctl daemon-reload
+sudo systemctl enable disable-swap.service
+
+# Proceed with Docker installation
+echo "Installing Docker and dependencies..."
+
+# Download Docker's GPG key
+wget -O - https://download.docker.com/linux/ubuntu/gpg > ./docker.key
+gpg --no-default-keyring --keyring ./docker.gpg --import ./docker.key
+gpg --no-default-keyring --keyring ./docker.gpg --export > ./docker-archive-keyring.gpg
+sudo mv ./docker-archive-keyring.gpg /etc/apt/trusted.gpg.d/
+
+# Add the Docker repository and install Docker
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt update -y
+sudo apt install -y docker-ce git wget curl socat
+
+# Install cri-dockerd
+VER=$(curl -s https://api.github.com/repos/Mirantis/cri-dockerd/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//g')
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
+tar xzvf cri-dockerd-${VER}.amd64.tgz
+sudo mv cri-dockerd/cri-dockerd /usr/local/bin/
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
+wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
+sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
+sudo sed -i -e 's,/usr/bin/cri-dockerd,/usr/local/bin/cri-dockerd,' /etc/systemd/system/cri-docker.service
+sudo systemctl daemon-reload
+sudo systemctl enable cri-docker.service
+sudo systemctl enable --now cri-docker.socket
+
+# Add the GPG key for Kubernetes
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+# Add the Kubernetes repository
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Update the repository and install Kubernetes packages
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+
+# Hold the versions so they won't accidentally upgrade
+sudo apt-mark hold docker-ce kubelet kubeadm kubectl
+
+# Enable iptables bridge
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Sysctl params required by setup
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sudo sysctl --system
+
+# Initialize Kubernetes master node using the control plane IP
+sudo kubeadm init --apiserver-advertise-address=${CONTROL_PLANE_IP} --cri-socket unix:///var/run/cri-dockerd.sock
+
+# Set up kubectl for the current user
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Set up Calico network
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/tigera-operator.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.28.2/manifests/custom-resources.yaml -O
+kubectl create -f custom-resources.yaml
+
+# Check nodes
+kubectl get nodes
